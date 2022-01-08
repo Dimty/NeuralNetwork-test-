44ecb2d787af5135747d55b009e59778f9fa9a59
Dimty <nikitin-dmitriyy@mail.ru>
2022-01-08 19:41:07 +0600

    Doshel do nahoshdeni9 oshibki

--- NeuronNet/NeuralNetwork.cs
+++ NeuronNet/NeuralNetwork.cs
@@ -6,43 +6,137 @@ namespace NeuralNet
 {
 	public class NeuralNetwork
 	{
-		public NeuralNetwork()
+        private int amountOfInputNeurons;
+        private int amountOfOutputNeurons;
+        private int[] amountOfHiddenNeurons;
+        public int AmountOfInputNeurons
+        {
+            get => amountOfInputNeurons;
+            set
+            {
+                if (value is 0) throw new Exception("The number of input neurons can't be zero");
+                else amountOfInputNeurons = value;
+            }
+        }
+        public int AmountOfOutputNeurons
+        {
+            get => amountOfOutputNeurons;
+            set
+            {
+                if (value is 0) throw new Exception("The number of output neurons can't be zero");
+                else amountOfOutputNeurons = value;
+            }
+        }
+        public int[] AmountOfHiddenNeurons
+        {
+            get => amountOfHiddenNeurons;
+            set => amountOfHiddenNeurons= value;
+        }
+        public List<List<Neuron>> layers = new List<List<Neuron>>();
+		public NeuralNetwork(int input,int output,params int[] hiddens)
 		{
-
+            AmountOfInputNeurons = input;
+            AmountOfOutputNeurons = output;
+            AmountOfHiddenNeurons = hiddens;
+            CreateLayers();
 		}
 
-		private void CreateHiddenLayers()
-		{
-            
-		}
-
-        public double[] Predict()
+        public void Training(double[,] inputParams, int[] expected,int epoch = 5000, double learningRate = 1.0d)
         {
-            
+            for (int q = 0; q < epoch; q++)
+            {
+                for (int i = 0; i < inputParams.Length; i++)
+                {
+                    var input = new double[inputParams.GetLength(1)];
+                    for (int j = 0; j < input.Length; j++)
+                    {
+                        input[j] = inputParams[i, j];
+                    }
+                    var result = Predict(input);
+                }
+            }    
         }
-
-        private void CreateInputAndOutputLayers()
-		{
-			
-        }
-
-        private static void CreateLayer()
-        {
-        }
-
-        public void Training()
-        {
-            
-        }
-
+        //ÍÓÆÍÎ ÍÀÉÒÈ ÎØÈÁÊÓ expect - actual
         private void BackPropagation()
         {
             
         }
+
+        public double[] Predict(double[] input)
+        {
+            SetInputValues(input);
+            for (int layer = 0; layer < layers.Count - 1; layer++)
+            {
+                double sum = 0.0d;
+                for (int weight = 0; weight < layers[layer + 1].Count; weight++)
+                {
+                    for (int neur = 0; neur < layers[layer].Count; neur++)
+                    {
+                        var part1 = layers[layer][neur].Output;
+                        var part2 = layers[layer][weight].listOfWeight[weight];
+                        sum += part1 * part2;
+                    }
+                    layers[layer + 1][weight].Output = sum;
+                }
+            }
+            return layers.Last().Select(x => x.Output).ToArray();
+        }
+
+        private void SetInputValues(double[] input)
+        {
+            for (int i = 0; i < amountOfInputNeurons; i++)
+            {
+                layers[0][i].Output = input[i];
+            }
+        }
+
+        private void CreateLayers()
+        {
+            CreateInputLayer();
+            CreateHiddenLayers();
+            CreateOutputLayer();
+        }
+
+
+
         private double[] TrainingPredict()
         {
             
         }
 
+		private void CreateHiddenLayers()
+		{
+            
+            for (int layer = 0; layer < amountOfHiddenNeurons.Length; layer++)
+            {
+                var list = new List<Neuron>();
+                for (int neu = 0; neu < amountOfHiddenNeurons[layer]; neu++)
+                {
+                    if(layer!=amountOfHiddenNeurons.Length-1) list.Add(new Neuron(TypeOfNeuron.Hidden,amountOfHiddenNeurons[layer+1]));
+                    else list.Add(new Neuron(TypeOfNeuron.Hidden, amountOfOutputNeurons));
+                }
+                layers.Add(list);
+            }
+		}
+        private void CreateInputLayer()
+		{
+            int count = amountOfOutputNeurons;
+            var list = new List<Neuron>();
+            if (amountOfHiddenNeurons.Length != 0) count = amountOfHiddenNeurons[0];
+            for (int i = 0; i < amountOfInputNeurons; i++)
+            {
+                list.Add(new Neuron(TypeOfNeuron.Input,count));
+            }
+            layers.Add(list);
+        }
+        private void CreateOutputLayer()
+        {
+            var list = new List<Neuron>();
+            for (int i = 0; i < amountOfOutputNeurons; i++)
+            {
+                list.Add(new Neuron(TypeOfNeuron.Output,0));
+            }
+            layers.Add(list);
+        }
 	}
 }
--- NeuronNet/Neuron.cs
+++ NeuronNet/Neuron.cs
@@ -5,15 +5,32 @@ namespace NeuralNet
 {
     public class Neuron
     {
-       
-        public Neuron()
+        public double exception = 0.0d;
+        public TypeOfNeuron type;
+        public List<double> listOfWeight = new List<double>();
+        private double output = 0.0d;
+        public double Output
         {
-
+            get => output;
+            set
+            {
+                if (type != TypeOfNeuron.Input) output = Sigmoid(value);
+                else output = value;
+            }
         }
-        private void GenerateRandomValueWeight()
+        public Neuron(TypeOfNeuron type,int count)
         {
+            this.type = type;
+            if(type!=TypeOfNeuron.Output) GenerateRandomValueWeight(count);
         }
-        public static double Sigmoid(double x)
+        private void GenerateRandomValueWeight(int count)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                listOfWeight.Add(new Random().NextDouble());
+            }
+        }
+        private static double Sigmoid(double x)
         {
             return (double)1 / (1 + Math.Exp(-x));
         }
